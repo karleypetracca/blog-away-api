df98e34cd878b69dbb512e72717ea4085d66f581
Karley Petracca <karleypetracca@gmail.com>
2020-04-02 13:05:46 -0400

    Fix: add CORS package and delete unused packages

--- app.js
+++ app.js
@@ -3,24 +3,27 @@ const express = require("express"),
 	cookieParser = require("cookie-parser"),
 	logger = require("morgan"),
 	es6Renderer = require("express-es6-template-engine"),
-	session = require("express-session"),
-	FileStore = require("session-file-store")(session);
+	cors = require("cors");
 
 const app = express();
+
+const corsOptions = {
+	origin: "*",
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	preflightContinue: false,
+	optionsSuccessStatus: 204,
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Headers":
+		"Origin, X-Requested-With, Content-Type, Accept"
+};
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(
-	session({
-		secret: "known",
-		resave: false,
-		saveUninitialized: true,
-		is_logged_in: false
-	})
-);
+
+app.use(cors(corsOptions));
 
 app.engine("html", es6Renderer);
 app.set("views", "./views");
