6124a44458ed464143bc7148e5d3fc7c5332a006
Karley Petracca <karleypetracca@gmail.com>
2020-04-02 11:46:13 -0400

    Feat: set up api calls in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,46 @@
 const express = require("express"),
-	router = express.Router();
+	router = express.Router(),
+	BlogModel = require("../models/BlogModel");
 
 /* GET home page. */
 router.get("/", function(req, res) {
-	res.render("index", { title: "Express" });
+	res.render("index", { title: "Welcome to the React-Blog API" });
 });
 
-// api test
-router.get("/api", async (req, res) => {
-	let data = (await drinkModel.getRandomCocktail()) || "";
+/* JSON all posts */
+router.get("/api/posts", async (req, res) => {
+	let data = await BlogModel.getAllPosts();
+
+	res.json(data);
+});
+
+/* JSON one post */
+router.get("/api/post/:post_id?", async (req, res) => {
+	const { post_id } = req.params;
+	let data = await BlogModel.getOnePost(post_id);
+
+	res.json(data);
+});
+
+/* JSON all comments */
+router.get("/api/comments/:post_id?", async (req, res) => {
+	const { post_id } = req.params;
+	let data = await BlogModel.getAllComments(post_id);
+
+	res.json(data);
+});
+
+/* JSON all authors */
+router.get("/api/authors", async (req, res) => {
+	let data = await BlogModel.getAllAuthors();
+
+	res.json(data);
+});
+
+/* JSON one author */
+router.get("/api/author/:author_id?", async (req, res) => {
+	const { author_id } = req.params;
+	let data = await BlogModel.getOneAuthor(author_id);
 
 	res.json(data);
 });
